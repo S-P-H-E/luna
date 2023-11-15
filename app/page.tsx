5ab49cc78b26cd3bf5c67a766f89b2a2da46f682
S-P-H-E <sphe.g.personal@gmail.com>
2023-11-16 00:34:47 +0200

    edge

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home(){
       <h1 className="text-4xl font-semibold">Please wait</h1>
       <div className='text-lg flex gap-2 items-center justify-center text-[#79797d]'>
         <p>Returning to page</p>
-        <BiLoader className="animate-spin" />
+        <BiLoader className="animate-spin"/>
       </div>
     </div>
   )
